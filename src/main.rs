d3c57f179f476ed3e3c21237851cc9b2baa94d8e
Takashiidobe <idobetakashi@gmail.com>
2025-05-24 15:56:06 -0400

    clean up tokenizer verbosity

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,38 @@
 use lc1::{codegen::Codegen, interpreter::Interpreter, parser, tokenizer::Lexer};
 use std::{env, fs, process};
 
+fn usage_and_exit(prog_name: &str, msg: &str) -> ! {
+    eprintln!("{}", msg);
+    eprintln!("Usage: {} [--interpreter | --asm] <input_file>", prog_name);
+    process::exit(1);
+}
+
 fn main() {
     let mut args = env::args();
-    let prog_name = args.next().unwrap_or_else(|| "lc1".into());
+    let prog_name = args.next().unwrap_or("lc1".to_string());
 
-    let mode = match args.next() {
-        Some(m) if m == "--interpreter" || m == "--asm" => m,
-        Some(other) => {
-            eprintln!("Unknown mode `{}`. Use --interpreter or --asm.", other);
-            eprintln!("Usage: {} [--interpreter | --asm] <input_file>", prog_name);
-            process::exit(1);
-        }
-        None => {
-            eprintln!("Missing mode. Use --interpreter or --asm.");
-            eprintln!("Usage: {} [--interpreter | --asm] <input_file>", prog_name);
-            process::exit(1);
-        }
-    };
+    let mode = args
+        .next()
+        .unwrap_or_else(|| usage_and_exit(&prog_name, "Missing mode."));
+    if mode != "--interpreter" && mode != "--asm" {
+        usage_and_exit(&prog_name, &format!("Unknown mode `{}`.", mode));
+    }
 
-    let filename = match args.next() {
-        Some(f) => f,
-        None => {
-            eprintln!("Missing input file.");
-            eprintln!("Usage: {} [--interpreter | --asm] <input_file>", prog_name);
-            process::exit(1);
-        }
-    };
+    let filename = args
+        .next()
+        .unwrap_or_else(|| usage_and_exit(&prog_name, "Missing input file."));
 
-    let src = match fs::read_to_string(&filename) {
-        Ok(s) => s,
-        Err(e) => {
-            eprintln!("Error reading `{}`: {}", filename, e);
-            process::exit(1);
-        }
-    };
+    let src = fs::read_to_string(&filename).unwrap_or_else(|e| {
+        eprintln!("Error reading `{}`: {}", filename, e);
+        process::exit(1);
+    });
 
-    let mut lexer = Lexer::new(&src);
-    let tokens = lexer.lex();
-    let mut parser = parser::Parser::new(tokens);
-    let ast = parser.parse_program();
+    let tokens = Lexer::new(&src).lex();
+    let ast = parser::Parser::new(tokens).parse_program();
 
     match mode.as_str() {
-        "--interpreter" => {
-            let mut interpreter = Interpreter::default();
-            interpreter.run(&ast);
-        }
-        "--asm" => {
-            let asm = Codegen::default().run(&ast);
-            println!("{}", asm);
-        }
-        _ => unreachable!(), // we've already validated
+        "--interpreter" => Interpreter::default().run(&ast),
+        "--asm" => println!("{}", Codegen::default().run(&ast)),
+        _ => unreachable!(),
     }
 }
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,6 +1,9 @@
+#[cfg(test)]
+use serde::{Deserialize, Serialize};
+
 #[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(test, derive(Serialize, Deserialize))]
 pub enum Token {
-    // Keywords
     If,
     Else,
     Fn,
@@ -9,31 +12,21 @@ pub enum Token {
     Return,
     Print,
     Struct,
-
-    // Identifiers and literals
     Ident(String),
     Int(i64),
     Str(String),
-
-    // Arithmetic
     Plus,
     Minus,
     Star,
     Slash,
-
-    // Operators
     Assign,
-    Arrow, // '->'
-
-    // Relational Ops
-    Lt,
-    Le,
+    Arrow,
     Eq,
     Ne,
+    Lt,
+    Le,
     Gt,
     Ge,
-
-    // Delimiters
     LBracket,
     RBracket,
     LParen,
@@ -44,8 +37,13 @@ pub enum Token {
     Colon,
     Semi,
     Dot,
-
     Eof,
+}
+
+pub struct Lexer<'input> {
+    input: &'input str,
+    pos: usize,
+    ch: Option<char>,
 }
 
 impl<'input> Lexer<'input> {
@@ -62,23 +60,15 @@ impl<'input> Lexer<'input> {
     pub fn lex(&mut self) -> Vec<Token> {
         let mut tokens = Vec::new();
         loop {
-            let tok = self.next_token();
-            tokens.push(tok.clone());
-            if tok == Token::Eof {
+            let token = self.next_token();
+            tokens.push(token.clone());
+            if token == Token::Eof {
                 break;
             }
         }
         tokens
     }
-}
 
-pub struct Lexer<'input> {
-    input: &'input str,
-    pos: usize,
-    ch: Option<char>,
-}
-
-impl Lexer<'_> {
     fn read_char(&mut self) {
         self.ch = self.input[self.pos..].chars().next();
         if let Some(c) = self.ch {
@@ -91,177 +81,57 @@ impl Lexer<'_> {
     }
 
     fn skip_whitespace(&mut self) {
-        while matches!(self.ch, Some(c) if c.is_whitespace()) {
+        while self.ch.is_some_and(|c| c.is_whitespace()) {
             self.read_char();
         }
     }
 
-    fn read_identifier(&mut self) -> String {
-        let mut ident = String::new();
+    fn read_while<F>(&mut self, mut pred: F) -> String
+    where
+        F: FnMut(char) -> bool,
+    {
+        let mut buf = String::new();
         while let Some(c) = self.ch {
-            if c.is_alphanumeric() || c == '_' {
-                ident.push(c);
+            if pred(c) {
+                buf.push(c);
                 self.read_char();
             } else {
                 break;
             }
         }
-        ident
+        buf
     }
 
-    fn read_number(&mut self) -> i64 {
-        let mut num_str = String::new();
-        while let Some(c) = self.ch {
-            if c.is_ascii_digit() {
-                num_str.push(c);
-                self.read_char();
-            } else {
-                break;
-            }
+    fn read_identifier(&mut self) -> Token {
+        let ident = self.read_while(|c| c.is_alphanumeric() || c == '_');
+        match ident.as_str() {
+            "if" => Token::If,
+            "else" => Token::Else,
+            "fn" => Token::Fn,
+            "for" => Token::For,
+            "let" => Token::Let,
+            "return" => Token::Return,
+            "print" => Token::Print,
+            "struct" => Token::Struct,
+            _ => Token::Ident(ident),
         }
-        num_str.parse().unwrap()
     }
 
-    pub fn next_token(&mut self) -> Token {
-        self.skip_whitespace();
-
-        // two‐char arrow
-        if self.ch == Some('-') && self.peek_char() == Some('>') {
-            self.read_char();
-            self.read_char();
-            return Token::Arrow;
-        }
-
-        if self.ch == Some('=') && self.peek_char() == Some('=') {
-            self.read_char();
-            self.read_char();
-            return Token::Eq;
-        }
-        if self.ch == Some('!') && self.peek_char() == Some('=') {
-            self.read_char();
-            self.read_char();
-            return Token::Ne;
-        }
-        if self.ch == Some('<') && self.peek_char() == Some('=') {
-            self.read_char();
-            self.read_char();
-            return Token::Le;
-        }
-        if self.ch == Some('>') && self.peek_char() == Some('=') {
-            self.read_char();
-            self.read_char();
-            return Token::Ge;
-        }
-
-        let tok = match self.ch {
-            Some('=') => {
-                self.read_char();
-                Token::Assign
-            }
-            Some('+') => {
-                self.read_char();
-                Token::Plus
-            }
-            Some('-') => {
-                self.read_char();
-                Token::Minus
-            }
-            Some('*') => {
-                self.read_char();
-                Token::Star
-            }
-            Some('/') => {
-                self.read_char();
-                Token::Slash
-            }
-            Some('<') => {
-                self.read_char();
-                Token::Lt
-            }
-            Some('>') => {
-                self.read_char();
-                Token::Gt
-            }
-            Some('(') => {
-                self.read_char();
-                Token::LParen
-            }
-            Some(')') => {
-                self.read_char();
-                Token::RParen
-            }
-            Some('[') => {
-                self.read_char();
-                Token::LBracket
-            }
-            Some(']') => {
-                self.read_char();
-                Token::RBracket
-            }
-            Some('{') => {
-                self.read_char();
-                Token::LBrace
-            }
-            Some('}') => {
-                self.read_char();
-                Token::RBrace
-            }
-            Some(',') => {
-                self.read_char();
-                Token::Comma
-            }
-            Some(':') => {
-                self.read_char();
-                Token::Colon
-            }
-            Some(';') => {
-                self.read_char();
-                Token::Semi
-            }
-            Some('.') => {
-                self.read_char();
-                Token::Dot
-            }
-            Some('"') => {
-                let lit = self.read_string();
-                Token::Str(lit)
-            }
-            Some(c) if c.is_ascii_digit() => {
-                let n = self.read_number();
-                Token::Int(n)
-            }
-            Some(c) if c.is_alphabetic() || c == '_' => {
-                let ident = self.read_identifier();
-                match ident.as_str() {
-                    "if" => Token::If,
-                    "else" => Token::Else,
-                    "fn" => Token::Fn,
-                    "for" => Token::For,
-                    "let" => Token::Let,
-                    "return" => Token::Return,
-                    "print" => Token::Print,
-                    "struct" => Token::Struct,
-                    other => Token::Ident(other.to_string()),
-                }
-            }
-            None => Token::Eof,
-            Some(c) => panic!("Unexpected character: {:?}", c),
-        };
-
-        tok
+    fn read_number(&mut self) -> Token {
+        let num = self.read_while(|c| c.is_ascii_digit());
+        Token::Int(num.parse().unwrap())
     }
 
-    fn read_string(&mut self) -> String {
-        self.read_char();
-        let mut s = String::new();
+    fn read_string(&mut self) -> Token {
+        self.read_char(); // skip opening "
+        let mut result = String::new();
         while let Some(c) = self.ch {
             if c == '"' {
                 break;
-            }
-            if c == '\\' {
-                if let Some(n) = self.peek_char() {
+            } else if c == '\\' {
+                if let Some(esc) = self.peek_char() {
                     self.read_char();
-                    s.push(match n {
+                    result.push(match esc {
                         'n' => '\n',
                         't' => '\t',
                         '"' => '"',
@@ -269,19 +139,70 @@ impl Lexer<'_> {
                         other => other,
                     });
                     self.read_char();
-                    continue;
                 }
+            } else {
+                result.push(c);
+                self.read_char();
             }
-            s.push(c);
-            self.read_char();
         }
-        self.read_char();
-        s
+        self.read_char(); // skip closing "
+        Token::Str(result)
+    }
+
+    pub fn next_token(&mut self) -> Token {
+        self.skip_whitespace();
+
+        macro_rules! punct {
+            ($single:expr => $tok:expr) => {{
+                self.read_char();
+                $tok
+            }};
+        }
+
+        if let Some(tok) = match (self.ch, self.peek_char()) {
+            (Some('-'), Some('>')) => Some(Token::Arrow),
+            (Some('='), Some('=')) => Some(Token::Eq),
+            (Some('!'), Some('=')) => Some(Token::Ne),
+            (Some('<'), Some('=')) => Some(Token::Le),
+            (Some('>'), Some('=')) => Some(Token::Ge),
+            _ => None,
+        } {
+            self.read_char();
+            self.read_char();
+            return tok;
+        }
+
+        match self.ch {
+            Some('=') => punct!('=' => Token::Assign),
+            Some('+') => punct!('+' => Token::Plus),
+            Some('-') => punct!('-' => Token::Minus),
+            Some('*') => punct!('*' => Token::Star),
+            Some('/') => punct!('/' => Token::Slash),
+            Some('<') => punct!('<' => Token::Lt),
+            Some('>') => punct!('>' => Token::Gt),
+            Some('(') => punct!('(' => Token::LParen),
+            Some(')') => punct!(')' => Token::RParen),
+            Some('[') => punct!('[' => Token::LBracket),
+            Some(']') => punct!(']' => Token::RBracket),
+            Some('{') => punct!('{' => Token::LBrace),
+            Some('}') => punct!('}' => Token::RBrace),
+            Some(',') => punct!(',' => Token::Comma),
+            Some(':') => punct!(':' => Token::Colon),
+            Some(';') => punct!(';' => Token::Semi),
+            Some('.') => punct!('.' => Token::Dot),
+            Some('"') => self.read_string(),
+            Some(c) if c.is_ascii_digit() => self.read_number(),
+            Some(c) if c.is_alphabetic() || c == '_' => self.read_identifier(),
+            Some(c) => panic!("Unexpected character: {:?}", c),
+            None => Token::Eof,
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use insta::assert_yaml_snapshot;
+
     use super::*;
 
     #[test]
@@ -293,23 +214,9 @@ mod tests {
             }
             greet("world");
         "#;
-        let mut lx = Lexer::new(src);
-        let mut toks = Vec::new();
-        loop {
-            let t = lx.next_token();
-            if t == Token::Eof {
-                toks.push(t);
-                break;
-            }
-            toks.push(t);
-        }
-        assert!(toks.contains(&Token::Fn));
-        assert!(toks.contains(&Token::Ident("greet".into())));
-        assert!(toks.contains(&Token::Str("Hello, ".into())));
-        assert!(toks.contains(&Token::Plus));
-        assert!(toks.contains(&Token::Print));
-        assert!(toks.contains(&Token::LBrace));
-        assert!(toks.contains(&Token::RBrace));
-        assert!(toks.contains(&Token::Eof));
+        let mut lexer = Lexer::new(src);
+        let tokens = lexer.lex();
+
+        assert_yaml_snapshot!(tokens);
     }
 }
